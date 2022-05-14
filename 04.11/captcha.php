43f0affffc09c2606355d8c85d4876dc05212176
Crzq <41958119+Crzq@users.noreply.github.com>
2022-05-14 20:24:02 +0800

    Add files via upload

--- 04.11/captcha.php
+++ 04.11/captcha.php
@@ -1,5 +1,5 @@
 <?php
-
+// echo phpinfo();
 include 'functions.php';
 
 if (!isset($_SESSION)) {
@@ -9,15 +9,12 @@ if (!isset($_SESSION)) {
 $width = 65;
 $height = 20;
 $image = imagecreate($width, $height);
-$bg_color = imagecolorallocate($image, 0, 0,0);
+$bg_color = imagecolorallocate($image, 0x33, 0x66, 0xff);
 $text = random_text(5);
 $font = 5;
-$x = imagesx($image) / 2 - strlen($text) * imagefontwidth($font) / 2;
-$y = imagesy($image) / 2 - imagefontheight($font) / 2;
-$fg_color = imagecolorallocate($image, 0xff, 0xff, 0xff);
-imagestring($image, $font, $x, $y, $text, $fg_color);
+$fg_color = imagecolorallocate($image, 255, 255, 255);
+imagestring($image, $font, 10, 2, $text, $fg_color);
 $_SESSION['captcha'] = $text;
-header('Content-type:image/png'); 
+header('Content-type:image/png');
 imagepng($image);
 imagedestroy($image);
-?>
